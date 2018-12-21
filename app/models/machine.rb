1c4e2a59f620890c68e07eba90b3dbb742517c78
Mary Goodhart <marya.grace5@gmail.com>
2018-12-21 10:56:32 -0700

    trying active record

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,4 +4,8 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :machine_snacks
   has_many :snacks, through: :machine_snacks
+
+  def self.average_price
+    joins(machine_snacks: :snacks).group(machine_id: :id).pluck(:price)
+  end
 end
--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,26 +1,8 @@
 require 'rails_helper'
 
-describe Machine, type: :model do
+describe Owner, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :location }
-    it { should belong_to :owner }
-  end
-  describe 'relationships' do
-    it {should have_many :machine_snacks}
-    it {should have_many(:snacks).through(:machine_snacks)}
-  end
-  describe 'Class method' do
-    describe 'should return average price of all snacks' do
-      it '.average_price' do
-        sam = Owner.create(name: "Sam's Snacks")
-        machine_1 = sam.machines.create(location: "Don's Mixed Drinks")
-        snack_1 = machine_1.snacks.create(name: "hot tamales", price: "5")
-        snack_1 = machine_1.snacks.create(name: "reeses", price: "10")
-        snack_1 = machine_1.snacks.create(name: "gummis", price: "15")
-        average_price_of_items = "10"
-
-        expect(Machine.average_price).to eq(average_price_of_items)
-      end
-    end
+    it { should validate_presence_of :name }
+    it { should have_many :machines }
   end
 end
