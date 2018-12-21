496a9b448a134d2e6760c4994f0d261350b49412
Mary Goodhart <marya.grace5@gmail.com>
2018-12-21 10:49:09 -0700

    adds average price test

--- spec/features/user_can_see_vending_machine_snacks_spec.rb
+++ spec/features/user_can_see_vending_machine_snacks_spec.rb
@@ -5,11 +5,21 @@ describe 'as a visitor to the machines show page' do
     sam = Owner.create(name: "Sam's Snacks")
     machine_1 = sam.machines.create(location: "Don's Mixed Drinks")
     snack_1 = machine_1.snacks.create(name: "hot tamales", price: "5")
-    # machine_snack_1 = MachineSnack.create(machine_id: machine_1, snack_id: snack_1)
 
     visit machine_path(machine_1)
 
     expect(page).to have_content("Candy: #{snack_1.name}")
     expect(page).to have_content("Price: #{snack_1.price}")
+  end
+  it 'should see the average price for all snacks' do
+    sam = Owner.create(name: "Sam's Snacks")
+    machine_1 = sam.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = machine_1.snacks.create(name: "hot tamales", price: "5")
+    snack_1 = machine_1.snacks.create(name: "reeses", price: "10")
+    snack_1 = machine_1.snacks.create(name: "gummis", price: "15")
+
+    visit machine_path(machine_1)
+
+    expect(page).to have_content("Average Price: #{machine_1.average_price}")
   end
 end
